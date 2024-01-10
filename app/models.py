4ce371e9ec8df36bc89f77b3ed01ba8296db0866
Jiliene Circelli <jilienecircelli@gmail.com>
2024-01-10 17:32:55 -0500

    updating models.py

--- app/models.py
+++ app/models.py
@@ -8,3 +8,23 @@ class Task(db.Model):
     completed = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+
+    def __repr__(self):
+        return f"<Task {self.id}|{self.title}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "completed": self.completed,
+            "dateCreated": self.date_created
+        }
